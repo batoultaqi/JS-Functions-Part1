2759957831e2c5f30e35f7de24f9e4fd0f004847
batoultaqi <batoul.t@outlook.com>
2024-11-04 17:48:23 +0300

    first commit

---
+++ functions1.js
@@ -0,0 +1,18 @@
+
+function printName() {
+    console.log ("Batoul");
+
+
+    
+}
+printName ();
+
+function printAge (birthYear) {
+
+    const age = 2024 - birthYear;
+    console.log ("Your age is: " + age)
+}
+ printAge (1997)
+
+
+function prinAgeAndName () {}
